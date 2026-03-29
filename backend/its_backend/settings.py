bfd449bbee5e6f3b06defe0f10a8a78131498eff
Himansu1729 <162155576+Himansu1729@users.noreply.github.com>
2026-03-30 01:30:08 +0530

    Add new CORS origin to allowed origins

--- backend/its_backend/settings.py
+++ backend/its_backend/settings.py
@@ -77,9 +77,9 @@ CORS_ALLOWED_ORIGINS = [
     o.strip()
     for o in os.getenv(
         'CORS_ALLOWED_ORIGINS',
-        'http://localhost:5173,http://127.0.0.1:5173',
-        'https://its-factorisation-new.vercel.app/'
+        'http://localhost:5173,http://127.0.0.1:5173,https://its-factorisation-new.vercel.app/'
     ).split(',')
+    if o.strip() # This extra check prevents empty strings if there's a trailing comma
 ]
 CORS_ALLOW_CREDENTIALS = True
 
