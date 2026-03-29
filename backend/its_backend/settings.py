2a2df8ba24eb6cd07e3581b41a2bc1128644deed
Himansu1729 <162155576+Himansu1729@users.noreply.github.com>
2026-03-30 01:17:38 +0530

    Update settings.py

--- backend/its_backend/settings.py
+++ backend/its_backend/settings.py
@@ -77,7 +77,8 @@ CORS_ALLOWED_ORIGINS = [
     o.strip()
     for o in os.getenv(
         'CORS_ALLOWED_ORIGINS',
-        'http://localhost:5173,http://127.0.0.1:5173'
+        'http://localhost:5173,http://127.0.0.1:5173',
+        'https://its-factorisation-new.vercel.app/'
     ).split(',')
 ]
 CORS_ALLOW_CREDENTIALS = True
